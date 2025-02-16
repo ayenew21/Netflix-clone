b1263c97526bd067c322a1ac89b4027dfaa0290d
Ayenew Dagnaw <learn4396@gmail.com>
2025-02-16 15:53:12 +0300

    Axios and requests created

---
+++ src/utils/requests.jsx
@@ -0,0 +1,14 @@
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const requests = {
+  originals: `/discover/tv?api_key=${api_key}&language=en-US&with_networks=213`,
+  trending: `/trending/all/week?api_key=${api_key}&language=en-US`,
+  top_rated: `/tv/top_rated?api_key=${api_key}&language=en-US&page=1`,
+  Action: `/discover/movie?api_key=${api_key}&language=en-US&with_genres=28`,
+  Adventure: `/discover/movie?api_key=${api_key}&language=en-US&with_genres=12`,
+  Animation: `/discover/movie?api_key=${api_key}&language=en-US&with_genres=16`,
+  Documentary: `/discover/movie?api_key=${api_key}&language=en-US&with_genres=99`,
+  Drama: `/discover/movie?api_key=${api_key}&language=en-US&with_genres=18`,
+};
+
+export default requests;
